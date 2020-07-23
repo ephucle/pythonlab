788f1a5e68aa2896636a0134e20ff19a8eff8969
ephucle <lephuchoang@gmail.com>
2020-07-23 11:29:11 +0700

    update card.py for excise

--- card.py
+++ card.py
@@ -13,7 +13,7 @@ class Card(object):
 		#return str(self.rank_names[self.rank]) + " of " +  str(self.suit_names[self.suit])
 		#return '%s of %s' % (Card.rank_names[self.rank], Card.suit_names[self.suit])
 		#return '%s of %s' % (self.rank_names[self.rank], self.suit_names[self.suit])
-		return '%d %d' % (self.suit, self.rank)
+		return '%d %d [%s %s]' % (self.suit, self.rank, Card.suit_names[self.suit], Card.rank_names[self.rank] )
 	
 	
 	def __lt__(self, other):
@@ -77,49 +77,100 @@ class Deck:
 				#print(f"self.cards[{j+1}] = {self.cards[j+1]} self.cards[{j}] = {self.cards[j]} ")
 				if self.cards[j+1] < self.cards[j] : 
 					self.cards[j], self.cards[j+1] = self.cards[j+1], self.cards[j] 
+	def move_cards(self, hand, num):
+		for i in range(num):
+			hand.add_card(self.pop_card())
+	def deal_hands(self, no_of_hand, no_of_card_per_hand):
+		list_of_hand = []
+		for i in range(no_of_hand):
+			#khoi tao new hand
+			hand = Hand(f"Hand{i}")
+			#move card to hand
+			self.move_cards(hand, no_of_card_per_hand)
+			#add vao list
+			list_of_hand.append(hand)
+		return list_of_hand
+def main():
+	#dung ham main, de co the import, ma ko chay lai full code
+	queen_of_diamonds = Card(1, 12)
+	
+	print(queen_of_diamonds.suit)
+	print(queen_of_diamonds.rank)
+	print(Card.suit_names)
+	print(Card.rank_names)
+	print(queen_of_diamonds)
+	
+	print("Test compare:")
+	card1 = Card(2, 11)
+	card2 = Card(3, 13)
+	card3 = Card(2, 11)
+	card4 = Card(0, 13)
+	card5 = Card(2, 13)
+	print(f"Compare {card1} < {card2}: {card1 < card2}")
+	print(f"Compare {card1} > {card2}: {card1 > card2}")
+	print(f"Compare {card1} = {card3}: {card1 == card3}")
+	print(f"Compare {card1} > {card4}: {card1 > card4}")
+	print(f"Compare {card5} > {card1}: {card5 > card1}")
+	#sys.exit()
+	
+	deck = Deck()
+	print("Bo bai/Deck:")
+	print (deck)
+	
+	
+	
+	#xao bai
+	print("Tien hanh xao bai...")
+	deck.shuffle()
+	
+	##rut 13 la
+	#print("Tien hanh rut ra 13 la...")
+	#for i in range(13):
+	#	card = deck.pop_card()
+	#	print(i, card)
+	print("Deck after suffle:", "*"*50)
+	print(deck)
+	print(len(deck.cards))
+	
+	print("Sort the deck by Deck.sort method...")
+	deck.sort()
+	print("deck after sort", "*"*50)
+	print(deck)
+	print(len(deck.cards))
 
-queen_of_diamonds = Card(1, 12)
 
-print(queen_of_diamonds.suit)
-print(queen_of_diamonds.rank)
-print(Card.suit_names)
-print(Card.rank_names)
-print(queen_of_diamonds)
+class Hand(Deck):
+	"""Represents a hand of playing cards. that is, the set of cards held by one player"""
+	def __init__(self, label=''):
+		"the init method for Hands should initialize cards with an empty list."
+		self.cards = []
+		self.label = label
+def main2():
+	#hand = Hand('new hand')
+	#print (hand.cards)
+	#print (hand.label)
+	
+	#deck = Deck()
+	#card = deck.pop_card()
+	#hand.add_card(card)
+	#print (hand)
+	
+	##test move card.
+	#deck.move_cards(hand, 3)
+	#print("all card in hand after move:")
+	#print(hand)  #luc nay hand se co 4 card
+	
+	print("Test deal_hands")
+	deck = Deck()
 
-print("Test compare:")
-card1 = Card(2, 11)
-card2 = Card(3, 13)
-card3 = Card(2, 11)
-card4 = Card(0, 13)
-card5 = Card(2, 13)
-print(f"Compare {card1} < {card2}: {card1 < card2}")
-print(f"Compare {card1} > {card2}: {card1 > card2}")
-print(f"Compare {card1} = {card3}: {card1 == card3}")
-print(f"Compare {card1} > {card4}: {card1 > card4}")
-print(f"Compare {card5} > {card1}: {card5 > card1}")
-#sys.exit()
+	#deal card to 5 hand, each hand 2 crash
+	list_of_hand = deck.deal_hands(5, 2)
+	print(list_of_hand)
+	print("length list_of_hand:", len(list_of_hand))
+	for hand in list_of_hand:
+		print("#"*10)
+		print(hand.label)
+		print(hand)
 
-deck = Deck()
-print("Bo bai/Deck:")
-print (deck)
-
-
-
-#xao bai
-print("Tien hanh xao bai...")
-deck.shuffle()
-
-##rut 13 la
-#print("Tien hanh rut ra 13 la...")
-#for i in range(13):
-#	card = deck.pop_card()
-#	print(i, card)
-print("Deck after suffle:", "*"*50)
-print(deck)
-print(len(deck.cards))
-
-print("Sort the deck by Deck.sort method...")
-deck.sort()
-print("deck after sort", "*"*50)
-print(deck)
-print(len(deck.cards))
+if __name__ == "__main__":
+	main2()
