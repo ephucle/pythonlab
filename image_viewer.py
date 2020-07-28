17d44f7ad367e9791455bd19eeb111c022b9ef72
ephucle <lephuchoang@gmail.com>
2020-07-28 10:08:02 +0700

    update image_viewer
    show image
    auto scan all image in a folder
    auto rotate by left click on canvas

--- image_viewer.py
+++ image_viewer.py
@@ -1,31 +1,14 @@
-#from __future__ import print_function
-#from swampy.Gui import *
 from swampy.Gui import Gui
-
+import collections
 from PIL import Image,ImageTk
-
-#help(ImageTk)
 
 import sys, os
 from tkinter import PhotoImage
-
-
-
-
-
 
 #>>> import swampy.Gui as Gui
 #>>> dir(Gui)
 #['ALL', 'BBox', 'BOTTOM', 'Callable', 'CanvasTransform', 'E', 'END', 'Gui', 'GuiCanvas', 'Item', 'LEFT', 'N', 'Point', 'RIGHT', 'RotateTransform', 'S', 'ScaleTransform', 'SwirlTransform', 'TOP', 'Transform', 'W', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'flatten', 'get_options', 'gui_example', 'main', 'math', 'override', 'pair', 'pairiter', 'pop_options', 'remove_options', 'split_options', 'sys', 'tk_example', 'tkinter', 'underride', 'widget_demo']
 
-#>>> import tkinter as tk
-#>>> dir(tk)
-#['ACTIVE', 'ALL', 'ANCHOR', 'ARC', 'BASELINE', 'BEVEL', 'BOTH', 'BOTTOM', 'BROWSE', 'BUTT', 'BaseWidget', 'BitmapImage', 'BooleanVar', 'Button', 'CASCADE', 'CENTER', 'CHAR', 'CHECKBUTTON', 'CHORD', 'COMMAND', 'CURRENT', 'CallWrapper', 'Canvas', 'Checkbutton', 'DISABLED', 'DOTBOX', 'DoubleVar', 'E', 'END', 'EW', 'EXCEPTION', 'EXTENDED', 'Entry', 'Event', 'EventType', 'FALSE', 'FIRST', 'FLAT', 'Frame', 'GROOVE', 'Grid', 'HIDDEN', 'HORIZONTAL', 'INSERT', 'INSIDE', 'Image', 'IntVar', 'LAST', 'LEFT', 'Label', 'LabelFrame', 'Listbox', 'MITER', 'MOVETO', 'MULTIPLE', 'Menu', 'Menubutton', 'Message', 'Misc', 'N', 'NE', 'NO', 'NONE', 'NORMAL', 'NS', 'NSEW', 'NUMERIC', 'NW', 'NoDefaultRoot', 'OFF', 'ON', 'OUTSIDE', 'OptionMenu', 'PAGES', 'PIESLICE', 'PROJECTING', 'Pack', 'PanedWindow', ------'PhotoImage' ----, 'Place', 'RADIOBUTTON', 'RAISED', 'READABLE', 'RIDGE', 'RIGHT', 'ROUND', 'Radiobutton', 'S', 'SCROLL', 'SE', 'SEL', 'SEL_FIRST', 'SEL_LAST', 'SEPARATOR', 'SINGLE', 'SOLID', 'SUNKEN', 'SW', 'Scale', 'Scrollbar', 'Spinbox', 'StringVar', 'TOP', 'TRUE', 'Tcl', 'TclError', 'TclVersion', 'Text', 'Tk', 'TkVersion', 'Toplevel', 'UNDERLINE', 'UNITS', 'VERTICAL', 'Variable', 'W', 'WORD', 'WRITABLE', 'Widget', 'Wm', 'X', 'XView', 'Y', 'YES', 'YView', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__', '_cnfmerge', '_default_root', '_exit', '_flatten', '_join', '_magic_re', '_setit', '_space_re', '_splitdict', '_stringify', '_support_default_root', '_test', '_tkerror', '_tkinter', '_varnum', 'constants', 'enum', 'getboolean', 'getdouble', 'getint', 'image_names', 'image_types', 'mainloop', 're', 'sys', 'wantobjects']
-
-#tk.PhotoImage
-#class PhotoImage(Image)
-# |  Widget which can display images in PGM, PPM, GIF, PNG format.
-#PhotoImage reads a file and returns a PhotoImage object that Tkinter can display.
 
 root = "./"
 img_file_paths = []
@@ -40,42 +23,40 @@ for path, subdirs, files in os.walk(root):
 			except IOError:
 				pass
 print(img_file_paths)
-#sys.exit()
 
 g = Gui()
 canvas = g.ca(width=200,height=200)
-first_image = img_file_paths.pop()
-print(first_image)
-#photo = PhotoImage(file='python.gif')
-#photo = PhotoImage(file=first_image)
-#canvas.image([0,0], image=photo)
+image_path = './python_mini.GIF'
+label1 = g.la(text = image_path)
 
+#show image to canvas
+image = Image.open(image_path)
+photo = ImageTk.PhotoImage(image)  #convert to ImageTk
+myimg = canvas.image([0,0], image=photo)
 
-first_image_path = img_file_paths.pop()
-image = Image.open(first_image_path)
-photo2 = ImageTk.PhotoImage(image)  #convert to ImageTk
-#canvas.image([0,0], image=photo2)
-g.la(image=photo2)
-
-def show_next_image(event):
+#click event
+def callback(event):
+	print ("clicked at", event.x, event.y)
 	global img_file_paths
-	next_photo_path = img_file_paths.pop()
-	print(next_photo_path)
 	
-	image = Image.open(next_photo_path)
-	photo2 = ImageTk.PhotoImage(image)  #convert to ImageTk
-	#update image in canvas
-	g.la.config(image = photo2)
+	#rotate list of image
+	d = collections.deque(img_file_paths)
+	d.rotate(1)
+	img_file_paths= list(d)
+	next_image_path = img_file_paths[-1]
 	
+	label1['text'] = next_image_path
+	#update new image
+	image2 = Image.open(next_image_path)
+	global photo2   #very importance
+	photo2 = ImageTk.PhotoImage(image2)
+	canvas.itemconfigure(myimg, image=photo2) #myimg is origin image in canvas
 
-#https://effbot.org/tkinterbook/tkinter-events-and-bindings.htm
-#<Button-1> A mouse button is pressed over the widget. Button 1 is the leftmost button, button 2 is the middle button (where available), and button 3 the rightmost button. 
+canvas.bind("<Button-1>", callback)  #<Button-1>: is mouse left button
 
-canvas.bind("<Button-1>", show_next_image)
 
-#image = Image.open('python_mini.gif')
-#photo2 = ImageTk.PhotoImage(image)
-#g.la(image=photo2)
+
+
 
 g.mainloop()
 
