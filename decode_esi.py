1dfa02ac5442d2dcf30c2e73a0427b5b1c54a429
ephucle <lephuchoang@gmail.com>
2020-08-07 12:24:11 +0700

    Update decode_esi.py
    add --path option for dcgm folder specify
    
    extract modump from dcgm, auto matically

--- decode_esi.py
+++ decode_esi.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#version 1.1 07Aug2020.
 
 ######################################################################################################
 #support to run from cygwin only due to gpg does not work on WSL 
@@ -21,33 +22,33 @@
 #  -d, --du    Parsing ESI DU
 #  -r, --ru    Parsing ESI RU
 ######################################################################################################
+
 from zipfile import ZipFile
 import os,sys, datetime, subprocess
 import shutil
 import argparse
 
+#note: you can set your default dcgm path here, or using option --path to set dcgm folder path
+default_dcgm_path = "/cygdrive/c/working/02-Project/16-SKT_5G_Project/03-1-DCGM"
+
 parser = argparse.ArgumentParser(description='ESI decode')
 parser.add_argument(dest='nodename', type=str, help="nodename")
 
+parser.add_argument("--path", dest="dcgm_path", action="store", type=str, help="path of dcgm folder", default=default_dcgm_path)
 parser.add_argument('-d', '--du', dest='esi_du', action='store_true', help='Parsing ESI DU')
 parser.add_argument('-r', '--ru', dest='esi_ru', action='store_true', help='Parsing ESI RU')
 
 nodename = parser.parse_args().nodename
+root_path = parser.parse_args().dcgm_path
 esi_du = parser.parse_args().esi_du
 esi_ru = parser.parse_args().esi_ru
 
 print("nodename:", nodename)
-print("esi du flag:", esi_du)
-print("esi ru flag:", esi_ru)
+print(f"root path: {root_path}")
+#print("esi du flag:", esi_du)
+#print("esi ru flag:", esi_ru)
 
 
-#root_path = "/mnt/c/working/02-Project/16-SKT_5G_Project/03-1-DCGM"
-root_path = "/cygdrive/c/working/02-Project/16-SKT_5G_Project/03-1-DCGM"
-#nodename = "bs-yeonje-yeonjeb-GX19"
-
-#dcgm_file_path = "/mnt/c/working/02-Project/16-SKT_5G_Project/03-1-DCGM/bs-yeonje-yeonjeb-GX19_200804_120521_KST_MSRBS-N_CXP9024418-12_R69B43_dcgm.zip"
-
-#dcgm_file_path = "/cygdrive/c/working/02-Project/16-SKT_5G_Project/03-1-DCGM/bs-yeonje-yeonjeb-GX19_200804_120521_KST_MSRBS-N_CXP9024418-12_R69B43_dcgm.zip"
 
 dcgm_filepaths = [os.path.join(root_path, file) for file in os.listdir(root_path) if os.path.isfile(os.path.join(root_path, file)) and nodename in file]
 dcgm_filepaths.sort()
@@ -69,13 +70,13 @@ else:
 	print(f"Folder {target_folder_path} existed")
 	
 
-if not os.path.isfile(os.path.join(target_folder_path,"modump.zip")):
-	source = os.path.join(root_path, "modump.zip")
-	target = os.path.join(target_folder_path, "modump.zip")
-	shutil.copyfile(source, target)
-	print(f"Successful copy {source} to {target}")
-else:
-	print(f"File modump.zip existed in {target_folder_path}")
+#if not os.path.isfile(os.path.join(target_folder_path,"modump.zip")):
+#	source = os.path.join(root_path, "modump.zip")
+#	target = os.path.join(target_folder_path, "modump.zip")
+#	shutil.copyfile(source, target)
+#	print(f"Successful copy {source} to {target}")
+#else:
+#	print(f"File modump.zip existed in {target_folder_path}")
 
 
 
@@ -83,6 +84,16 @@ else:
 with ZipFile(dcgm_file_path) as myzip:
 	namelist = myzip.namelist()
 	#print(namelist)
+	
+	#extract modump
+	modump_filename = nodename + "_modump.zip"
+	myzip.extract(modump_filename, target_folder_path)
+	print(f"Successful extract {modump_filename} to {target_folder_path}")
+	global modump_path
+	modump_path = os.path.join(target_folder_path, modump_filename)
+	
+	
+	
 	logfiles_name = nodename + "_logfiles.zip"
 	myzip.extract(logfiles_name, target_folder_path)
 	print (f"Successful extract {logfiles_name} to {target_folder_path}")
@@ -97,11 +108,11 @@ with ZipFile(dcgm_file_path) as myzip:
 				du_esi_path = item
 			if "esi.ru_" in item:
 				ru_esi_path.append(item) 
-		
-		print("du esi path", du_esi_path) #rcslogs/esi.du1.20200804T030752+0000.tar.gz.gpg
+		if esi_du:
+			print("du esi path", du_esi_path) #rcslogs/esi.du1.20200804T030752+0000.tar.gz.gpg
 		temp , du_esi_filename = os.path.split(du_esi_path)
-		
-		print("ru esi path", ru_esi_path) #['rcslogs/esi.ru_05-F-AIR3239.20200804T030930+0000.tar.gz.gpg', 'rcslogs/esi.ru_02-C-AIR3239.20200804T031326+0000.tar.gz.gpg', 'rcslogs/esi.ru_01-B-AIR3239.20200804T031439+0000.tar.gz.gpg', 'rcslogs/esi.ru_00-A-AIR3239.20200804T031501+0000.tar.gz.gpg', 'rcslogs/esi.ru_04-E-AIR3239.20200804T031047+0000.tar.gz.gpg', 'rcslogs/esi.ru_03-D-AIR3239.20200804T031213+0000.tar.gz.gpg']
+		if esi_ru:
+			print("ru esi path", ru_esi_path) #['rcslogs/esi.ru_05-F-AIR3239.20200804T030930+0000.tar.gz.gpg', 'rcslogs/esi.ru_02-C-AIR3239.20200804T031326+0000.tar.gz.gpg', 'rcslogs/esi.ru_01-B-AIR3239.20200804T031439+0000.tar.gz.gpg', 'rcslogs/esi.ru_00-A-AIR3239.20200804T031501+0000.tar.gz.gpg', 'rcslogs/esi.ru_04-E-AIR3239.20200804T031047+0000.tar.gz.gpg', 'rcslogs/esi.ru_03-D-AIR3239.20200804T031213+0000.tar.gz.gpg']
 		
 		#extract ESI DU
 		if esi_du:
@@ -150,8 +161,8 @@ def decode_esi_by_gpg():
 	output_filepath = os.path.join(target_folder_path,nodename + "_script_output.log")
 	#find moshell path on WSL or cygwin
 	moshell_path = subprocess.getoutput('which moshell')
-	global modump_path
-	modump_path = os.path.join(target_folder_path, "modump.zip")
+	#global modump_path
+	#modump_path = os.path.join(target_folder_path, "modump.zip")
 	print (">>> moshell path:", moshell_path )
 	
 	full_script = moshell_path + " " + modump_path + " " + moshell_script_path + " | tee " + output_filepath
