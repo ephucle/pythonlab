d3c71fbb418deab74bf3503e0405425ecc87bf7c
ephucle <lephuchoang@gmail.com>
2021-05-23 13:17:11 +0700

    add function support package profile
    load many script at the same time
    support  create new package profile
    support add new package profile
    
    automatically and save time for profile package create

--- script_creater/fill_template.py
+++ script_creater/fill_template.py
@@ -11,7 +11,9 @@ import datetime
 from myfunc import ls_name, get_filename, get_file_name_ext
 
 
-version = "V03_20052021"
+version = "V04_20052023"
+#version = "V04_20052023": support package profile, create script with many template as a sametime, create package profile
+
 home_path = os.path.dirname(os.path.realpath(__file__))
 print(home_path)
 
@@ -220,6 +222,7 @@ def save_profile_press():
 	global template_filepath,input_file_path,var_foldersplit,var_mergefile
 	selected_sheet = om_var.get()
 	folder_tag = om_folder_var.get()
+	package_name = text_var_packagename.get()
 	print("save_profile_press!!")
 	#init a dict
 	data = {}
@@ -235,9 +238,44 @@ def save_profile_press():
 	
 	print("data to be save as below")
 	print(data)
+	
 	with open(os.path.join(home_path,'profile',profile_filename), 'w') as outfile:
 		json.dump(data, outfile)
-	text_var_status.set("Save profile to "+ profile_filename)
+	#text_var_status.set("Save profile to "+ profile_filename)
+	
+	#####################
+	data2 = {}
+	path,template_filename = os.path.split(template_filepath)
+	
+	package_filename = package_name + ".json"
+	package_filepath = os.path.join(home_path,'profile','profile_package',package_filename)
+	data2['package_name'] = package_name
+	
+	#data2['profiles_filepath'] = [template_filename]
+	
+	
+	print("package data to be save as below")
+	print(data2)
+	#neu file ko ton tai thi tao moi
+	if not os.path.exists(package_filepath):
+		data2['profiles_filepath'] = [template_filename]
+		with open(package_filepath, 'w') as outfile:
+			json.dump(data2, outfile)
+	else: #neu file co ton tai
+		with open(package_filepath) as json_file:
+			data_dict = json.load(json_file)
+			print(data_dict)
+		#profile_package_name = data_dict['package_name']
+		profiles_filepath = data_dict['profiles_filepath']
+		if template_filepath not in profiles_filepath:
+			profiles_filepath.append(template_filepath)
+		
+		data2['profiles_filepath'] = profiles_filepath
+	#save new package profile or update it with new data
+	with open(package_filepath, 'w') as outfile:
+		json.dump(data2, outfile)
+	text_var_status.set("Saved profile to: "+ profile_filename+" | Saved package to: "+ get_filename(package_filename))
+	#####################
 	
 	#update option menu profile with new profile
 	global om_profile_var
@@ -444,6 +482,160 @@ def mergefile_option_status_change():
 	print("mergefile_option_status_change ticked !!")
 	print(var_mergefile.get())
 
+
+def show_profile_package_to_textbox():
+	print("profile package option menu selected")
+	profile_package_filename_selected = om_profile_package_var.get()
+	print(profile_package_filename_selected)
+	clear_textbox()
+	print_to_textbox("Profile package selected: "+profile_package_filename_selected)
+	#open profile to load content, load back data to variable
+	global home_path
+	
+	with open(os.path.join(home_path,"profile","profile_package",profile_package_filename_selected)) as json_file:
+		data_dict = json.load(json_file)
+		print(data_dict)
+		profile_package_name = data_dict['package_name']
+		profiles_filepath = data_dict['profiles_filepath']
+		print(profile_package_name)
+		print(profiles_filepath)
+		print_to_textbox("profile_package_name "+profile_package_name)
+		
+		print_to_textbox("-----------------------------------------")
+		print_to_textbox("profile_filepaths: ")
+		for profile_filepath in profiles_filepath:
+			#print_to_textbox(profile_filepath)
+			print_to_textbox(get_filename(profile_filepath))
+
+	profile_package_select.configure(bg="green2")
+	text_var_status.set("STATUS: PACKAGE SELECTED")
+	loadprofile_package_button.configure(state='normal')
+
+
+def loadprofile_procedure(profile_filepath):
+	print("creating script for profile", profile_filepath)
+	#log_textbox.delete("1.0","end")
+	#print profile name to text box
+	print_to_textbox("creating script for profile:" + profile_filepath)
+	with open(profile_filepath) as json_file:
+		data_dict = json.load(json_file)
+		print(data_dict)
+		template_filepath = data_dict["template_filepath"]
+		input_file_path = data_dict["input_file_path"]
+		selected_sheet = data_dict["selected_sheet"]
+		folder_tag = data_dict["folder_tag"]
+		foldersplit = data_dict['foldersplit']
+		filenametag = data_dict['filenametag']
+		mergefile = data_dict['mergefile']
+		
+		print_to_textbox("Excel Input: "+get_filename(input_file_path))
+		print_to_textbox("Sheet Name: "+selected_sheet)
+		print_to_textbox("Template Input: "+get_filename(template_filepath))
+		print_to_textbox("Folder_tag: "+folder_tag)
+		print_to_textbox("foldersplit: "+str(foldersplit))
+		print_to_textbox("filenametag: "+str(filenametag))
+		print_to_textbox("mergefile: "+str(mergefile))
+		print_to_textbox("-------------------------")
+		
+		input_filename = get_filename(input_file_path)[:20]
+	
+	
+	print("selected_sheet:", selected_sheet)
+	df = pd.read_excel(input_file_path, header=0, sheet_name=selected_sheet)
+	print("SUMMARY DATA INPUT TABLE:")
+	print(df)
+	global column_header
+	column_headers = list(df.columns.values)
+	
+	
+	f = open(template_filepath, "r")
+	input_file_content = f.read()
+	
+	f.close()
+	
+	folder, template_filename = os.path.split(template_filepath)
+	
+	#find all variable in template
+	with open(template_filepath) as infile:
+		lines = infile.readlines()
+		
+		#find all variable string
+		var_set = set()
+		for index, line in enumerate(lines):
+			line = line.strip()
+			regex = '\$' + "(\w+)"
+			variables = re.findall(regex, line)
+
+			for item in variables:
+				var_set.add(item)
+			
+		print("----------------all variable string_from load profile--------------")
+		print(var_set)
+
+	#substitute data from each row of excel into to template
+	for index, row in df.iterrows():
+		data = {}
+		for column_name in var_set:
+			if column_name in column_headers:
+				#print(index,column_name,row[column_name])
+				data[column_name] = row[column_name]
+		print(data)
+		rowname = filenametag
+		
+		src = Template(input_file_content)
+		result = src.substitute(data)
+
+		#split script follow folder
+		templatename, ext = get_file_name_ext(template_filename)
+		if not foldersplit:
+			output_filepath = os.path.join(home_path, "output_script",templatename + "_" + str(row[rowname]) + "." + ext)
+		else:
+			#neu folder ko ton tai thi tao them folder
+			if not os.path.exists(os.path.join(home_path, "output_script",str(row[folder_tag]))):
+				os.mkdir(os.path.join(home_path, "output_script",row[folder_tag]))
+			
+			#rule dat script name, co row, co template name
+			output_filepath = os.path.join(home_path, "output_script",row[folder_tag], templatename + "_" + row[rowname] + "."+ ext)
+		
+		if mergefile:
+			output_text_file = open(output_filepath, "a")
+		else :
+			output_text_file = open(output_filepath, "w")
+		output_text_file.write(result)
+		output_text_file.close()
+		print("write successful", data, "to", output_filepath)
+		print_to_textbox("write successful script "+  output_filepath)
+
+#########################
+
+def loadprofile_package():
+	#cho vang cai nut de biet la bat dau ==> de troubleshooting
+	loadprofile_package_button.configure(bg = 'yellow')
+	print("loadprofile_package button press!!")
+	profile_package_filename_selected = om_profile_package_var.get()
+	print(profile_package_filename_selected)
+	clear_textbox()
+	print_to_textbox("Profile package selected: "+profile_package_filename_selected)
+	#open profile to load content, load back data to variable
+	global home_path
+	
+	with open(os.path.join(home_path,"profile","profile_package",profile_package_filename_selected)) as json_file:
+		data_dict = json.load(json_file)
+		print(data_dict)
+		profile_package_name = data_dict['package_name']
+		profiles_filepath = data_dict['profiles_filepath']
+		print(profile_package_name)
+		print(profiles_filepath)
+		print_to_textbox("profile_package_name "+profile_package_name)
+		print_to_textbox("-----------------------------------------")
+		print_to_textbox("profile_filepaths: ")
+		for profile_filepath in profiles_filepath:
+			print_to_textbox(profile_filepath)
+			loadprofile_procedure(profile_filepath)
+	
+	#cho xanh cai nut , de biet la chay thanh cong
+	loadprofile_package_button.configure(bg = 'green2')
+
 root = tk.Tk()
 root.title("SCRIPTING_"+version)
 #root.geometry("550x550")
@@ -456,6 +648,7 @@ text_var_inputpath = tk.StringVar()
 text_var_templatepath = tk.StringVar()
 text_var_status = tk.StringVar()
 text_var_status.set("STATUS: PLS SELECT EXCEL OR CHOOSE PROFILE")
+text_var_packagename = tk.StringVar()
 
 global om_folder_var
 om_folder_var = tk.StringVar(root)
@@ -500,6 +693,10 @@ create_button.configure(state='disabled')
 
 ##row6
 tk.Button(root, text ="06.BACKUP",     command = save_profile_press).grid(row=5, column=1)
+
+tk.Label(root, text="package_name").grid(row=5, column=2, sticky="w")
+entry_packagename = tk.Entry(root, textvariable = text_var_packagename)
+entry_packagename.grid(row=5, column=2, sticky="e")
 ##end row6
 
 ##row7
@@ -511,25 +708,41 @@ om_profile_var.set("07.Select a profile")
 profile_select = tk.OptionMenu(root, om_profile_var, *choices,command=lambda _: show_profile_to_textbox())
 #mymenu = OptionMenu(root, optionvar, *t, command=lambda _: update())
 profile_select.grid(row=6, column=2, sticky="ew", columnspan=2)
-
 #end row7
 
-#row8
-log_textbox = tk.Text(root, height=15)
-log_textbox.grid(row=7, column=2, sticky="ew",columnspan=2)
+##row8
+profile_package_files = ls_name(os.path.join(home_path,'profile','profile_package'))
+choices = profile_package_files
+om_profile_package_var = tk.StringVar(root)
+om_profile_package_var.set("07_1.Select a profile package")
+profile_package_select = tk.OptionMenu(root, om_profile_package_var, *choices,command=lambda _: show_profile_package_to_textbox())
+#mymenu = OptionMenu(root, optionvar, *t, command=lambda _: update())
+profile_package_select.grid(row=7, column=2, sticky="ew", columnspan=2)
 #end row8
 
 #row9
-loadprofile_button= tk.Button(root, text ="08.LOAD PROFILE & CREATE SCRIPT",     command = loadprofile)
-loadprofile_button.grid(row=8, column=2, sticky="ew", columnspan=2)
-loadprofile_button.configure(state='disabled')
-
+log_textbox = tk.Text(root, height=15)
+log_textbox.grid(row=8, column=2, sticky="ew",columnspan=2)
 #end row9
 
-##row10
+#row10
+loadprofile_button= tk.Button(root, text ="08.LOAD PROFILE & CREATE SCRIPT",     command = loadprofile)
+loadprofile_button.grid(row=9, column=2, sticky="ew", columnspan=2)
+loadprofile_button.configure(state='disabled')
+#end row10
+
+#row11
+loadprofile_package_button= tk.Button(root, text ="08_1.LOAD PROFILE PACKAGE & CREATE SCRIPT",     command = loadprofile_package)
+loadprofile_package_button.grid(row=10, column=2, sticky="ew", columnspan=2)
+loadprofile_package_button.configure(state='disabled')
+
+
+#end row11
+
+##row12
 status_label = tk.Label(root, textvariable =text_var_status,)
-status_label.grid(row=9, column=1, columnspan=3)
-##end row10
+status_label.grid(row=11, column=1, columnspan=3)
+##end row12
 
 
 
